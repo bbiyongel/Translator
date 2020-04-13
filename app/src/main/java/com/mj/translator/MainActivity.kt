9ac97ef3f0cf5a927e976c4455ae42206cbc3b0b
mjkim <eoqkrskfk94@naver.com>
2020-04-13 17:13:21 +0900

    material design

--- app/src/main/java/com/mj/translator/MainActivity.kt
+++ app/src/main/java/com/mj/translator/MainActivity.kt
@@ -27,11 +27,14 @@ public class MainActivity : AppCompatActivity() {
 
 
 
-        translatebtn.setOnClickListener {
 
-            val asyncTask = NaverTranslateTask()
-            asyncTask.execute(editText.text.toString())
-        }
+//        translatebtn.setOnClickListener {
+//
+//            val asyncTask = NaverTranslateTask()
+//            asyncTask.execute(editText.text.toString())
+//            val asyncTask2 = KakaoTranslateTask()
+//            asyncTask2.execute(editText.text.toString())
+//        }
     }
 
     inner class NaverTranslateTask() : AsyncTask<String, Void, String>() {
@@ -97,7 +100,7 @@ public class MainActivity : AppCompatActivity() {
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
 
-            Log.d("background result", result.toString())
+            Log.d("background result1", result.toString())
 
             val gson: Gson =  GsonBuilder().create();
             val parser = JsonParser();
@@ -110,6 +113,90 @@ public class MainActivity : AppCompatActivity() {
             //Log.d("result", items.getTranslatedText());
             //번역결과를 텍스트뷰에 넣는다.
             papago.text = items.translatedText
+
+
+
+
+
+
+        }
+
+
+    }
+
+    inner class KakaoTranslateTask() : AsyncTask<String, Void, String>() {
+
+
+        val clientId = "AwAUJEDlEenilEpdoQFZ"
+        val clientSecret = "wrHp5MN5uu"
+        val apikey = "ee05a8584607f80bc72156c4c7106750"
+
+        val sourceLang = "en"
+        val targetLang = "kr"
+
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+
+            val sourceText = params[0]
+            try {
+                val text = URLEncoder.encode(sourceText, "UTF-8")
+                val postParams = "src_lang=" + sourceLang + "&target_lang=" + targetLang + "&query=" + text;
+                val apiURL = "https://kapi.kakao.com/v1/translation/translate?"+postParams;
+                val url = URL(apiURL);
+                val con: HttpURLConnection = url.openConnection() as HttpURLConnection;
+                val userCredentails = apikey
+                val basicAuth = "KakaoAK " + userCredentails
+                con.setRequestProperty("Authorization", basicAuth);
+                con.requestMethod = "GET"
+                con.setRequestProperty("Content-Type", "application/x-www-for-urlencoded")
+                con.setRequestProperty("charset", "utf-8")
+                con.useCaches = false
+                con.doInput = true
+                con.doOutput = true
+                val responseCode = con.responseCode
+                var br: BufferedReader
+                if (responseCode == 200) { // 정상 호출
+                    br = BufferedReader(InputStreamReader(con.getInputStream()))
+                } else {  // 에러 발생
+                    br = BufferedReader(InputStreamReader(con.getErrorStream()))
+                }
+
+
+                var inputLine: String? = null
+                val response = StringBuffer();
+                while ({ inputLine = br.readLine(); inputLine }() != null) {
+                    response.append(inputLine)
+                }
+                br.close()
+                //System.out.println(response.toString());
+                return response.toString()
+
+
+            } catch (e:Exception){
+                //System.out.println(e);
+
+                return ""
+            }
+        }
+
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            Log.d("background result2", result.toString())
+
+            val gson: Gson =  GsonBuilder().create();
+            val parser = JsonParser();
+            val rootObj: JsonElement = parser.parse(result.toString())
+                .asJsonObject.get("translated_text")
+
+            val translation = rootObj.asJsonArray.get(0).asJsonArray.get(0).toString().replace("\"","")
+
+            kakao.text = translation
 
 
 
